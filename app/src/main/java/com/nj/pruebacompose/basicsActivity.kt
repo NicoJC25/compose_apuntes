5e0fee87e4a94b6d81df14c181a8514baf97e8ea
NicoJC25 <nejuez@misena.edu.co>
2024-06-12 11:01:04 -0500

    filas y columnas apuntes

---
+++ app/src/main/java/com/nj/pruebacompose/basicsActivity.kt
@@ -0,0 +1,79 @@
+package com.nj.pruebacompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class basicsActivity : ComponentActivity() { //Por si hay confusion, la clase se debe llamar igual que el archivo
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            mainFunction() //Main function en reemplazo a las lineas de codigo principales
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun mainFunction() { //Funcion principal
+    LazyColumn( //LazyColumn (Reemplazo de Recycler view o Scroll View)
+        modifier = Modifier //Modificador para la plantilla
+            .fillMaxSize() //Que ocupe to-do el espacio de la apliacion
+            .background(Color.Cyan) //De fondo, un color cyan
+    ) {
+        item {//Se debe de crear un item para las lazycolumns, dentro del item va to-do lo que va a contener esta lazy column
+            Image( //Imagen
+                modifier = Modifier //Tambien el modifier es como el que asigna las configuraciones a cada cosa
+                    .fillMaxWidth() //Que la imagen llene to-do el ancho
+                    .height(400.dp), //Y de alto, 400dp
+                painter = painterResource(id = R.drawable.logo_banana), //De donde se va a sacar la imagen
+                contentDescription = "Logo Banana", //Texto alterno
+            )
+            Text( //Ingresamos un texto
+                text = "Hola, Nicolas", //Texto como tal
+                fontSize = 32.sp, //Tamaño del texto
+                color = Color.White, //Color del texto
+                modifier = Modifier.fillMaxWidth(), //Modificador de que llene to-do el ancho
+                textAlign = TextAlign.Center //Para luego, alinearlo al centro de la pantalla
+            )
+            Text(text = "Texto de prueba de parrafo", color = Color.White) //Otro texto con color blanco
+            Text(text = "Segundo texto de prueba", color = Color.White) //Otro texto con color blanco
+            LazyRow( //LazyRow permite una fila (horizontal) fuera de la aplicacion, pudiendo deslizar a la izquierda o derecha dependiendo
+                horizontalArrangement = Arrangement.SpaceBetween, //Se le da una configuracion horizontal para que cada item este separado en misma cantidad de distancia
+                modifier = Modifier.fillMaxWidth().padding(16.dp) //Que ocupe el maximo de ancho, pero con separacion de 16dp a la izquierda y derecha
+            ) {
+                item{//Item, ya que los objetos tipo "Lazy" necesitan un item
+                    Text(text = "Hola soy Yo", color = Color.White) //Textos para comprobar que funcione bien
+                    Text(text = "Hola soy Yo", color = Color.White)
+                    Text(text = "Hola soy Yo", color = Color.White)
+                    Text(text = "Hola soy Yo", color = Color.White)
+                    Text(text = "Hola soy Yo", color = Color.White)
+                    Text(text = "Hola soy Yo", color = Color.White)
+                    Text(text = "Hola soy Yo", color = Color.White)
+                    Text(text = "Hola soy Yo", color = Color.White)
+                }
+            }
+        }
+
+    }
+}
